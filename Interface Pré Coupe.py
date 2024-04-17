ee79bb78f5480439804a3b40212641ed60a9aee9
Tsan-152 <166550571+Tsan-152@users.noreply.github.com>
2024-04-17 19:59:59 +0200

    Update Interface Pré Coupe.py

--- Interface Pré Coupe.py
+++ Interface Pré Coupe.py
@@ -36,11 +36,11 @@ class StrategySelectionPage(tk.Frame):
         self.return_callback = return_callback
         self.show_steps_selection = show_steps_selection
 
-        self.configure(bg="black")
+        self.configure(bg="white")
         self.pack(fill=tk.BOTH, expand=True)
 
         # Chargement de l'image
-        image_path = "vinyle_table_2024_FINAL_V1.png"
+        image_path = "vinyle_table_2024_FINAL_v1.png"
         self.image = tk.PhotoImage(file=image_path)
 
         # Création du cadre pour le premier bloc avec l'image en arrière-plan
@@ -68,8 +68,8 @@ class StrategySelectionPage(tk.Frame):
 
         # Création du titre de l'équipe sélectionnée
         team_color = "#FFFF99" if self.team == "Jaune" else "#99CCFF"
-        self.team_title_label = tk.Label(self.frame_with_buttons, text=self.team, font=("Helvetica", 30, "bold"), bg=team_color)
-        self.team_title_label.pack(fill=tk.BOTH, padx=10, pady=5)
+        self.team_title_label = tk.Label(self.frame_with_buttons, text=self.team, font=("Helvetica", 36, "bold"), bg=team_color)
+        self.team_title_label.pack(fill=tk.BOTH, padx=20, pady=10)
 
         # Création des boutons de sélection de stratégie
         self.buttons = []
@@ -79,14 +79,14 @@ class StrategySelectionPage(tk.Frame):
                 button_text = "Stratégie N°1 (MODIFIÉE)"
             elif self.team == "Bleue" and i == 1:
                 button_text = "Stratégie N°1 (MODIFIÉE)"
-            button = tk.Button(self.frame_with_buttons, text=button_text, font=("Helvetica",28, "bold"), command=lambda strategy_number=i: self.on_strategy_selected(strategy_number, json_filename), bd=2, relief="groove")
+            button = tk.Button(self.frame_with_buttons, text=button_text, font=("Helvetica", 24, "bold"), command=lambda strategy_number=i: self.on_strategy_selected(strategy_number, json_filename), bd=2, relief="groove")
             button.configure(bg=button_color)  # Pas de bordure
-            button.pack(fill="both", expand=True, padx=10, pady=5)
+            button.pack(fill="both", expand=True, padx=20, pady=10)
             self.buttons.append(button)
 
         # Création du bouton de retour à la page de sélection d'équipe
-        self.return_button = tk.Button(self.frame_with_buttons, text="Retour à la sélection d'équipe", font=("Helvetica", 27), bg="#FFDDC1", command=self.return_callback, bd=2, relief="groove")
-        self.return_button.pack(side=return_button_side, fill=tk.BOTH, padx=10, pady=5)
+        self.return_button = tk.Button(self.frame_with_buttons, text="Retour à la sélection d'équipe", font=("Helvetica", 24), bg="#FFDDC1", command=self.return_callback, bd=2, relief="groove")
+        self.return_button.pack(side=return_button_side, fill=tk.BOTH, padx=20, pady=10)
 
     def on_strategy_selected(self, strategy_number, json_filename):
         # Copie du fichier JSON dans le dossier DOSSIER
@@ -106,71 +106,74 @@ class StepsSelectionPage(tk.Frame):
         self.pack(fill=tk.BOTH, expand=True)
 
         # Chargement de l'image
-        image_path = "vinyle_table_2024_FINAL_V1.png"
+        image_path = "fond_ENSIMelec.png"
         self.image = tk.PhotoImage(file=image_path)
 
         # Création du cadre pour le premier bloc avec l'image en arrière-plan
-        self.frame_with_image = tk.Frame(self, bg="white", width=1980, height=1080, bd=2, relief="groove")  # Fond blanc
+        self.frame_with_image = tk.Frame(self, bg="black", width=1980, height=1080, bd=2, relief="groove")  # Fond blanc
         self.frame_with_image.pack(fill=tk.BOTH, expand=True)
 
         # Affichage de l'image en arrière-plan
         self.background_label = tk.Label(self.frame_with_image, image=self.image)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        # Création du cadre pour la succession des étapes
-        self.steps_frame = tk.Frame(self.frame_with_image, bg="#000080", bd=2, relief="groove")
-        self.steps_frame.place(relx=0.5, rely=0.65, anchor="center")
+        ## Création du cadre pour la succession des étapes
+        #self.steps_frame = tk.Frame(self.frame_with_image, bg="#000080", bd=2, relief="groove")
+        #self.steps_frame.place(relx=0.5, rely=0.65, anchor="center")
 
         # Création des cases Étape N°X ou les étapes modifiées
-        self.step_labels = []
-        if self.strategy_number == 1:
-            steps_text = [
-                "START",
-                "Rotation 3 Panneaux Solaires",
-                "Récup' 1ier Groupe Plantes Line",
-                "Dépose 1ier Groupe TOP Jardinière",
-                "Déplacement Pots Gênants SIDE",
-                "Récup' 2ième Groupe Plantes Line",
-                "Dépose 2ième Groupe SIDE Jardinère",
-                "Récup' Groupe Trash TOP",
-                "Back Zone",
-                "STOP"
-            ]
-        else:
-            steps_text = [f"Étape N°{i}" for i in range(1, 11)]
-        for step_text in steps_text:
-            step_label = tk.Label(self.steps_frame, text=step_text, font=("Helvetica", 24, "bold"), bg="#FFFF99", bd=2, relief="groove", width=40)  # Augmenter la largeur
-            step_label.pack(fill="both", padx=20, pady=10)
-            self.step_labels.append(step_label)
+        #self.step_labels = []
+        #if self.strategy_number == 1:
+        #    steps_text = [
+        #        "START",
+        #        "Rotation 3 Panneaux Solaires",
+        #        "Récup' 1ier Groupe Plantes Line",
+        #        "Dépose 1ier Groupe TOP Jardinière",
+        #        "Déplacement Pots Gênants SIDE",
+        #        "Récup' 2ième Groupe Plantes Line",
+        #        "Dépose 2ième Groupe SIDE Jardinère",
+        #        "Récup' Groupe Trash TOP",
+        #        "Back Zone",
+        #        "STOP"
+        #    ]
+        #else:
+        #    steps_text = [f"Étape N°{i}" for i in range(1, 11)]
+        #for step_text in steps_text:
+        #    step_label = tk.Label(self.steps_frame, text=step_text, font=("Helvetica", 24, "bold"), bg="#FFFF99", bd=2, relief="groove", width=40)  # Augmenter la largeur
+        #    step_label.pack(fill="both", padx=20, pady=10)
+        #    self.step_labels.append(step_label)
 
         # Création du bloc pour le nombre de points
-        self.points_block = tk.Frame(self.frame_with_image, bg="#000080", bd=2, relief="groove")  # Rouge pâle
-        self.points_block.place(relx=0.45, rely=0.1, relwidth=0.1, relheight=0.2)
+        self.points_block = tk.Frame(self.frame_with_image, bg="#7AC5CD", bd=2, relief="groove")  # Rouge pâle
+        self.points_block.place(relx=0.425, rely=0.21, relwidth=0.15, relheight=0.28)
 
         # Titre du bloc
-        self.points_label = tk.Label(self.points_block, text="Points", font=("Helvetica", 35, "bold"), bg="#FFE4E1")
+        self.points_label = tk.Label(self.points_block, text="Points", font=("Helvetica", 60, "bold"), bg="#FCFCFC")
         self.points_label.pack(fill=tk.BOTH, padx=10, pady=5)
 
         # Variable pour stocker le nombre de points
         self.points_counter_value = tk.StringVar()
-        self.points_counter_value.set("0")  # Initialisation à 0
+        self.points_counter_value.set("57")  # Initialisation à 0
 
         # Compteur initialisé à 0
-        self.points_counter = tk.Label(self.points_block, textvariable=self.points_counter_value, font=("Helvetica", 80), bg="#FFE4E1")
+        self.points_counter = tk.Label(self.points_block, textvariable=self.points_counter_value, font=("Helvetica", 150), bg="#FCFCFC")
         self.points_counter.pack(fill=tk.BOTH, padx=10, pady=5)
 
         # Création du bouton de retour à la page de sélection de stratégie
-        self.return_button = tk.Button(self.frame_with_image, text="Retour à la sélection de stratégie", font=("Helvetica", 24), bg="#FFDDC1", command=self.return_callback, bd=2, relief="groove")
-        self.return_button.place(relx=0.70, rely=0.265, anchor="center")    
+        self.return_button = tk.Button(self.frame_with_image, text="Retour à la sélection de stratégie", font=("Helvetica", 28), bg="#FFDDC1", command=self.return_callback, bd=2, relief="groove")
+        self.return_button.place(relx=0.78, rely=0.17, anchor="center")
+            
 
 class MainApplication(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Interface Graphique")
-        self.geometry("750x400")
+        self.attributes('-fullscreen', True)  # Lancement en plein écran
 
         self.team_selection_page = TeamSelectionPage(self, self.on_team_selected)
+
+    # Le reste de votre code reste inchangé
 
     def on_team_selected(self, team):
         self.team_selection_page.pack_forget()
